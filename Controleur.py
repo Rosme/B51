6373a79cee5388a9e8d178876ea4d06446c69270
Marc Grenier <mrcgrenier@gmail.com>
2012-12-06 23:23:28 -0500

    ajout de methode rutilisable, methode renommer pour plus de calrter et ajout de commentaire dans frameJeu

--- Controleur.py
+++ Controleur.py
@@ -48,7 +48,7 @@ class Controleur():
         if self.mouvement[4]:
             if self.jeu.joueur.tire(self.jeu.listeBalle, self.x, self.y):
                 balle = self.jeu.listeBalle[len(self.jeu.listeBalle)-1]
-                balle.posMatX,balle.posMatY=self.app.frameJeu.coord(balle.posMapX+(balle.veloX)*2,balle.posMapY+(balle.veloY)*2)
+                balle.posMatX,balle.posMatY=self.app.frameJeu.coordEcranAMatrice(balle.posMapX+(balle.veloX)*2,balle.posMapY+(balle.veloY)*2)
             else:
                 balle = self.jeu.listeBalle[len(self.jeu.listeBalle)-1]
                 self.jeu.listeBalle.remove(balle);
@@ -66,13 +66,13 @@ class Controleur():
         for i in self.jeu.listeBalle:
             i.bouge(self.jeu.joueur)
             if i.veloY<0 and i.veloX<0:
-                i.posMatX,i.posMatY=self.app.frameJeu.coord(i.posMapX+(i.veloX)*2,(i.posMapY+(i.veloY)*2)+30)
+                i.posMatX,i.posMatY=self.app.frameJeu.coordEcranAMatrice(i.posMapX+(i.veloX)*2,(i.posMapY+(i.veloY)*2)+30)
             elif i.veloY>0 and i.veloX>0:
-                i.posMatX,i.posMatY=self.app.frameJeu.coord((i.posMapX+(i.veloX)*2)+10,(i.posMapY+(i.veloY)*2)+10)
+                i.posMatX,i.posMatY=self.app.frameJeu.coordEcranAMatrice((i.posMapX+(i.veloX)*2)+10,(i.posMapY+(i.veloY)*2)+10)
             elif i.veloY<0 and i.veloX>0:
-                i.posMatX,i.posMatY=self.app.frameJeu.coord((i.posMapX+(i.veloX)*2)+40,(i.posMapY+(i.veloY)*2)+40)                
+                i.posMatX,i.posMatY=self.app.frameJeu.coordEcranAMatrice((i.posMapX+(i.veloX)*2)+40,(i.posMapY+(i.veloY)*2)+40)                
             else:
-                i.posMatX,i.posMatY=self.app.frameJeu.coord((i.posMapX+(i.veloX)*2)+25,(i.posMapY+(i.veloY)*2)+25)
+                i.posMatX,i.posMatY=self.app.frameJeu.coordEcranAMatrice((i.posMapX+(i.veloX)*2)+25,(i.posMapY+(i.veloY)*2)+25)
 
             if i.collision(liste, self.jeu.carte.s.salle):
                 temp.remove(i)
@@ -89,19 +89,14 @@ class Controleur():
     def actualiser(self):
         laMap=self.jeu.carte.s.salle
         
-        tempx=0
-        tempy=0
         tempx, tempy = self.jeu.joueur.bouge(self.mouvement)
         
-        tempMatX,tempMatY=self.app.frameJeu.coord(self.jeu.joueur.posMapX+tempx,self.jeu.joueur.posMapY+tempy)
+        tempMatX,tempMatY=self.app.frameJeu.coordEcranAMatrice(self.jeu.joueur.posMapX+tempx,self.jeu.joueur.posMapY+tempy)
         if laMap[tempMatY][tempMatX]== 'm' or laMap[tempMatY][tempMatX] == 'v' or laMap[tempMatY][tempMatX]== 'b' or laMap[tempMatY][tempMatX] == 'n':
             car=laMap[tempMatY][tempMatX]
             self.jeu.joueur.nomMap=self.jeu.carte.s.changementCarte(car)
             self.jeu.joueur=self.app.frameJeu.coordProchaineZone(self.jeu.carte.s,car,self.jeu.joueur)
-            self.app.frameJeu.effaceMap()
-            self.app.frameJeu.affichageMap(self.jeu.joueur,self.jeu.carte.s)
-            self.app.frameJeu.affichagePerso(self.jeu.joueur)
-
+            
         elif laMap[tempMatY][tempMatX]=='0' or laMap[tempMatY][tempMatX]=='2'  or laMap[tempMatY][tempMatX]=='q' or laMap[tempMatY][tempMatX]=='w':
             if laMap[tempMatY+1][tempMatX-1]!='1':
                 if tempx!=0 or tempy!=0:
@@ -109,7 +104,7 @@ class Controleur():
                     self.jeu.joueur.posMatY=tempMatY
                     self.jeu.joueur.posMapX+=tempx
                     self.jeu.joueur.posMapY+=tempy
-                    self.app.frameJeu.depl(tempx,tempy)
+                    self.app.frameJeu.deplScrollBar(tempx,tempy)
                     self.app.frameJeu.affichagePerso(self.jeu.joueur)
         
         if self.jeu.listeInterrupteur:
@@ -125,22 +120,13 @@ class Controleur():
         self.app.frameJeu.hudHaut.actualiser() 
         
         if self.jeu.joueur.race.vie==0:
-            self.jeu.joueur.posMapX,self.jeu.joueur.posMapY=self.app.frameJeu.posInitPerso()
+            #self.jeu.joueur.posMapX,self.jeu.joueur.posMapY=self.app.frameJeu.posInitPerso()
             self.jeu.joueur.mort()
             self.jeu.carte.s.salle=self.jeu.carte.s.dictMap[self.jeu.joueur.nomMap]
             
-            self.app.frameJeu.calculPositionDepart(self.jeu.carte.s,self.jeu.joueur)
-            self.app.frameJeu.calculPositionMilieu(self.jeu.carte.s,self.jeu.joueur)
-            
-            self.jeu.joueur.posMatX,self.jeu.joueur.posMatY=self.app.frameJeu.coord(self.jeu.joueur.posMapX,self.jeu.joueur.posMapY)
-
-            self.app.frameJeu.effaceMap()
-            
-            self.app.frameJeu.calculOffSet(self.jeu.joueur.posMapX,self.jeu.joueur.posMapY)
-            self.app.frameJeu.ajustOffSet()
-            
-            self.app.frameJeu.affichageMap(self.jeu.joueur,self.jeu.carte.s)
-            self.app.frameJeu.affichagePerso(self.jeu.joueur)
+            self.jeu.joueur=self.app.frameJeu.debutDePartie(self.jeu.joueur,self.jeu.carte.s)
+            self.app.frameJeu.changementDeMap(self.jeu.carte.s,self.jeu.joueur)
+           
             
     ############################# Méthodes en lien avec la création et la suppression d'éléments du modèle #############################
     def raceInfo(self, race):
