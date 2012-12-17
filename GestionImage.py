54d7aa551810b9370257864ab8fa86690d5945f0
Rosme <jeanseb.fauteux@gmail.com>
2012-12-17 14:11:22 -0500

    Test to fix file size

--- GestionImage.py
+++ GestionImage.py
@@ -17,7 +17,12 @@ class GestionImage():
         self.dictImage["gazon"]=tkinter.PhotoImage(file="assets/image/plancher_roche1.gif")
         self.dictImage["pers"]=tkinter.PhotoImage(file="assets/image/f1.gif")
         self.dictImage["coffre"]=tkinter.PhotoImage(file="assets/image/vie.gif")
-        #self.dictImage["feu"]=tkinter.PhotoImage(file="assets/image/feu.gif")
+        self.dictImage["simonJaune"]=tkinter.PhotoImage(file="assets/image/Simon/SimonJaune.gif")
+        self.dictImage["simonBleu"]=tkinter.PhotoImage(file="assets/image/Simon/SimonBleu.gif")
+        self.dictImage["simonVert"]=tkinter.PhotoImage(file="assets/image/Simon/SimonVert.gif")
+        self.dictImage["simonRouge"]=tkinter.PhotoImage(file="assets/image/Simon/SimonRouge.gif")
+        self.dictImage["feu"]=tkinter.PhotoImage(file="assets/image/feu.gif")
+        self.dictImage["eau"]=tkinter.PhotoImage(file="assets/image/eau.gif")
         
         #menu
         self.dictImage["backgroundImage"] = tkinter.PhotoImage(file='assets/Image/Prometheus_1.gif',width=1024,height=768)
