2027dddaf836aa29169f139fbb6b1e0fd94dcd34
Rosme <jeanseb.fauteux@gmail.com>
2013-01-15 13:02:37 -0500

    Fix on balle

--- Controleur.py
+++ Controleur.py
@@ -136,7 +136,7 @@ class Controleur():
         #ecouteur lié à la souris
         self.app.frameJeu.map.bind("<Button-1>", self.peseTire)
         self.app.frameJeu.map.bind("<ButtonRelease-1>", self.relacheTire)
-        #self.app.frameJeu.map.bind("<B1-Motion>", self.tireCoord)
+        self.app.frameJeu.map.bind("<B1-Motion>", self.tireCoord)
     
     ############################# Méthodes en lien avec les events de l'utilisateur #############################
     def peseKeyGestion(self, event):
